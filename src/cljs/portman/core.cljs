cfe95e9f40ead4f16e5037b4afe9dd717aaaf0f9
Chris Perkins <cperkins@rallydev.com>
2015-04-02 08:49:37 -0600

    Sweet, sweet pie

--- src/cljs/portman/core.cljs
+++ src/cljs/portman/core.cljs
@@ -27,7 +27,6 @@
   (let [selected (reagent/atom (first @data))]
     (events/subscribe :table/select
                       (fn [item]
-                        (.log js/console "selected:" (:name item))
                         (reset! selected item)))
     (fn [config data]
       (let [config @config
@@ -62,18 +61,7 @@
            {:header "Value"
             :render :value}]}))
 
-(def table-data
-  (reagent/atom
-   (vec (for [i (shuffle (range 1 8)) :let [v (str "Top-level-" i)]]
-          {:name     v
-           :value    i
-           :size     (+ 2 i)
-           :key      v
-           :children (for [j (range 1 (- 5 i)) :let [v (str i "-" j)]]
-                       {:name  v
-                        :value v
-                        :size  (+ 2 j)
-                        :key   v})}))))
+
 
 
 (defn deg->rad [deg]
@@ -83,53 +71,97 @@
   (* 180 (/ rad Math/PI)))
 
 (defn pt-on-circle
-  "Return the point on the perimeter of the circle centered at [cx
-  cy], with radius r, that is `angle` degrees clockwise from
+  "Return the point on the perimeter of the circle centered
+  at [cx,cy], with radius r, that is `angle` degrees clockwise from
   center-top."
   [[cx cy] r angle]
   (let [rads (deg->rad angle)]
     [(+ cx (* r (Math/sin rads)))
      (- cy (* r (Math/cos rads)))]))
 
-(defn arc [[cx cy :as c] ri ro angle start-angle]
+(defn arc-path
+  "Calculate and return the path of a circular arc."
+  [[cx cy :as c] ri ro angle start-angle]
   (let [a0 start-angle
         a1 (+ start-angle angle)
         [x0 y0] (pt-on-circle c ro a0)
         [x1 y1] (pt-on-circle c ro a1)
         [x2 y2] (pt-on-circle c ri a1)
         [x3 y3] (pt-on-circle c ri a0)]
-    [:path {:d     (string/join
-                    " "
-                    ["M" x0 y0
-                     "A" ro ro 0 (if (> angle 180) 1 0) 1 x1 y1
-                     "L" x2 y2
-                     "A" ri ri 0 (if (> angle 180) 1 0) 0 x3 y3
-                     "Z"] )
-            :style {:stroke "#fff" :fill "#ccc" :stroke-width 1}}]))
+    (string/join
+     " "
+     ["M" x0 y0
+      "A" ro ro 0 (if (> angle 180) 1 0) 1 x1 y1
+      "L" x2 y2
+      "A" ri ri 0 (if (> angle 180) 1 0) 0 x3 y3
+      "Z"] )))
 
 (defn pie [config data]
-  (let []
-    (fn [config data]
-      (let [total (reduce + (map :size @data))]
-        [:svg {:width 600 :height 600}
-         [:g
-          (let [start-angle (atom 0)]
-            (cur-for
-             [item data
-              :let [angle (-> @item :size (/ total) (* 360))]]
-             (let [elt ^{:key (:key @item)} [arc [300 300] 50 250 angle @start-angle]]
-               (swap! start-angle + angle)
-               elt)))]]))))
+  [:g
+   (let [[X Y]       (:center config)
+         inner-r     (:inner-r config 0)
+         ring-widths (let [w (:ring-widths config 100)]
+                       (if (number? w) (repeat w) (concat [nil] w (repeat (last w)))))
+         depth       (:depth config 1)
+         outer-r     (+ inner-r (nth ring-widths depth))
+         full-angle  (:full-angle config 360)
+         start-angle (atom (:start-angle config 0))
+         total       (reduce + (map #(:size % 1) @data))
+         palette     (-> js/d3 .-scale .category20b)]
+     (doall
+      (for [{:keys [size key] :as item} @data]
+        (let [angle (-> size (/ total) (* full-angle))
+              path  (arc-path [X Y] inner-r outer-r angle @start-angle)
+              children (when-let [ch (:children item)]
+                         [pie (merge config
+                                     {:full-angle  angle
+                                      :inner-r     outer-r
+                                      :depth       (inc depth)
+                                      :start-angle @start-angle})
+                          (atom ch)])]
+          (swap! start-angle + angle)
+          (palette (str "junk" key))    ; skip some colors
+          ^{:key key}
+          [:g [:path {:d     path
+                      :style {:stroke       "#fff"
+                              :fill         (palette key)
+                              :stroke-width 1}}]
+           children]))))])
 
 
-(defonce pie-config
-  {})
+(def pie-config
+  {:center      [600 10]
+   :inner-r     50
+   :ring-widths [150 125 100 80 50 30 20]
+   :full-angle  180
+   :start-angle 90})
+
+(defn gen-test-data [num depth prob]
+  (when (pos? depth)
+    (vec
+     (for [i (range num)]
+       {:key  i
+        :size (Math/random)
+        :children (when (< (Math/random) prob)
+                    (gen-test-data num (dec depth) prob))}))))
+
+(def test-data
+  (reagent/atom
+   (gen-test-data 3 5 0.95)))
+
+
+(def CTG (reagent/adapt-react-class js/React.addons.CSSTransitionGroup))
+
+(defn animated []
+  [CTG {:transition-name "foo"}
+   [:h1 "Animated"]])
 
 (defn app []
   [:div
-   [:h1 "Portman"]
-   ;[table table-config table-data]
-   [pie pie-config table-data]
+   [animated]
+   ;[table table-config test-data]
+   [:svg {:width 1200 :height 800}
+    [pie pie-config test-data]] 
    ])
 
 (defn mount-root []
